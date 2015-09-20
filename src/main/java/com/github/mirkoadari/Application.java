e1f194f649ac8b3f7638595a0e65916ca043d993
Mirko Adari <mirko.adari@live.com>
2015-09-20 13:23:55 +0300

    Aded Spark Java.

---
+++ src/main/java/com/github/mirkoadari/Application.java
@@ -0,0 +1,11 @@
+package com.github.mirkoadari;
+
+import static spark.Spark.*;
+
+public class Application {
+
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+}
