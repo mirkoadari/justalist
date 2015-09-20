5188f1f923a07ec853520cc33e5b2554796b2ace
Mirko Adari <mirko.adari@live.com>
2015-09-20 13:55:17 +0300

    Added Founation.

--- src/main/java/com/github/mirkoadari/Application.java
+++ src/main/java/com/github/mirkoadari/Application.java
@@ -5,6 +5,8 @@ import static spark.Spark.*;
 public class Application {
 
     public static void main(String[] args) {
+        staticFileLocation("/public");
+
         get("/hello", (req, res) -> "Hello World");
     }
 
